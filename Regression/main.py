e45446d9f2c73fd1f2b6c3d7d55590ad6e067f3e
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-03-22 13:13:05 +0100

    cleaned the data

--- Regression/main.py
+++ Regression/main.py
@@ -3,11 +3,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#read data from cars.csv
-cars = pd.read_csv('../Data/cars.csv')
 
-#print cars
-print(cars)
+cars_df = pd.read_csv('../Data/cars.csv')
 
+cars_df_copy = cars_df.copy()
+
+cars_df_copy['horsepower'] = pd.to_numeric(cars_df_copy['horsepower'], errors='coerce')
+
+mean_horsepower = round(cars_df_copy['horsepower'].mean(), 1)
+
+cars_df_copy['horsepower'].fillna(mean_horsepower, inplace=True)
+
+cars_df_copy.to_csv('../Data/cars_cleaned.csv', index=False)
+
+print(cars_df_copy)
 
 #%%
