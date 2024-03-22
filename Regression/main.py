8cae0a96037ac3e342817c298721e0f7ae3f83e1
ZackO <zo@pyra.dk>
2024-03-22 12:41:31 +0100

    read csv

---
+++ Regression/main.py
@@ -0,0 +1,13 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#read data from cars.csv
+cars = pd.read_csv('../Data/cars.csv')
+
+#print cars
+print(cars)
+
+
+#%%
